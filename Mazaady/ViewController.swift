632bc7c05e7b1104ed50881e19a32f2e9342f034
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-03-25 10:15:58 +0200

    Initial Commit

---
+++ Mazaady/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mazaady
+//
+//  Created by Walid Ahmed on 25/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MazaadyTests/MazaadyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MazaadyTests.swift
+//  MazaadyTests
+//
+//  Created by Walid Ahmed on 25/03/2025.
+//
+
+import Testing
+@testable import Mazaady
+
+struct MazaadyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
